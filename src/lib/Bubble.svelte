8bd2c9fdb12d6fd9fecad395f6061ac95d6d21c9
BT <71487741+qriousdev@users.noreply.github.com>
2024-05-27 17:27:28 -0700

    update font family

--- src/lib/Bubble.svelte
+++ src/lib/Bubble.svelte
@@ -33,7 +33,8 @@
 		border-radius: 100rem;
 		padding: 1rem 2rem;
 		box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.1);
-		font-family: inherit;
+		font-family: ui-sans-serif, system-ui, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji',
+			'Segoe UI Symbol', 'Noto Color Emoji';
 		font-size: 15px;
 		font-weight: 600;
 		border: none;
